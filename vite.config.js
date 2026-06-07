0871b5b0512b6af876b239ab164877c121ef8f0b
mahmoudesadi-byte <mahmoud.e.sadi@gmail.com>
2026-06-07 13:32:15 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  publicDir: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: 'index.html'
     }
-  },
-  publicDir: 'public'
+  }
 })
